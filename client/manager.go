f444fcc334c79cdda16474dd236e8e8c4a9dda2d
Matthew Hartstonge <matt@linc-ed.com>
2017-05-24 16:09:21 +1200

    :arrow_up: Client: Add comments to exported interfaces

--- client/manager.go
+++ client/manager.go
@@ -2,12 +2,14 @@ package client
 
 import "github.com/ory/fosite"
 
+// Manager provides a generic interface to clients in order to build a DataStore
 type Manager interface {
 	Storage
 
 	Authenticate(id string, secret []byte) (*Client, error)
 }
 
+// Storage conforms to fosite.Storage and provides methods
 type Storage interface {
 	fosite.Storage
 
