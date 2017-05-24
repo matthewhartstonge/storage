2ae099399370dc2fe2c29d5dfd70b3cc3283f1b6
Matthew Hartstonge <matt@linc-ed.com>
2017-05-24 16:08:52 +1200

    :arrow_up: Client: move methods around to match interface order

--- client/manager_mongo.go
+++ client/manager_mongo.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"context"
 	"github.com/imdario/mergo"
 	"github.com/ory/fosite"
 	"github.com/pkg/errors"
@@ -14,21 +15,8 @@ type MongoManager struct {
 	Hasher fosite.Hasher
 }
 
-// GetConcreteClient finds a Client based on ID and returns it, if found in Mongo.
-func (m *MongoManager) GetConcreteClient(id string) (*Client, error) {
-	collection := m.DB.C("clients").With(m.DB.Session.Copy())
-	defer collection.Database.Session.Close()
-
-	client := &Client{}
-	err := collection.Find(bson.M{"_id": id}).One(client)
-	if err != nil {
-		return nil, errors.WithStack(err)
-	}
-	return client, nil
-}
-
 // GetClient returns a Client if found by an ID lookup.
-func (m *MongoManager) GetClient(id string) (fosite.Client, error) {
+func (m *MongoManager) GetClient(ctx context.Context, id string) (fosite.Client, error) {
 	return m.GetConcreteClient(id)
 }
 
@@ -63,6 +51,19 @@ func (m *MongoManager) UpdateClient(c *Client) error {
 		return errors.WithStack(err)
 	}
 	return nil
+}
+
+// GetConcreteClient finds a Client based on ID and returns it, if found in Mongo.
+func (m *MongoManager) GetConcreteClient(id string) (*Client, error) {
+	collection := m.DB.C("clients").With(m.DB.Session.Copy())
+	defer collection.Database.Session.Close()
+
+	client := &Client{}
+	err := collection.Find(bson.M{"_id": id}).One(client)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return client, nil
 }
 
 // Authenticate compares a client secret with the client's stored hashed secret
