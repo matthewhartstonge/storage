026f1032baa8d90fbe1b5b14c6ec8cedaa08c11a
Matthew Hartstonge <matt@linc-ed.com>
2017-05-26 11:41:59 +1200

    :arrow_up: request: underscore context in AccessTokenStorage implementations as the context is not used

--- request/request_oauth2_access_token_storage.go
+++ request/request_oauth2_access_token_storage.go
@@ -8,16 +8,16 @@ import (
 /* These functions provide a concrete implementation of fosite.AccessTokenStorage */
 
 // CreateAccessTokenSession creates a new session for an Access Token in mongo
-func (m *MongoManager) CreateAccessTokenSession(ctx context.Context, signature string, request fosite.Requester) (err error) {
+func (m *MongoManager) CreateAccessTokenSession(_ context.Context, signature string, request fosite.Requester) (err error) {
 	return m.createSession(signature, request, mongoCollectionAccessTokens)
 }
 
 // GetAccessTokenSession returns a session if it can be found by signature in mongo
-func (m MongoManager) GetAccessTokenSession(ctx context.Context, signature string, session fosite.Session) (request fosite.Requester, err error) {
+func (m MongoManager) GetAccessTokenSession(_ context.Context, signature string, session fosite.Session) (request fosite.Requester, err error) {
 	return m.findSessionBySignature(signature, session, mongoCollectionAccessTokens)
 }
 
 // DeleteAccessTokenSession removes an Access Tokens current session from mongo
-func (m *MongoManager) DeleteAccessTokenSession(ctx context.Context, signature string) (err error) {
+func (m *MongoManager) DeleteAccessTokenSession(_ context.Context, signature string) (err error) {
 	return m.deleteSession(signature, mongoCollectionAccessTokens)
 }
