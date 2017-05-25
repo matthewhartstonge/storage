2fc2cb688b37187e4c046a52a47edd5ada3e9b78
Matthew Hartstonge <matt@linc-ed.com>
2017-05-26 11:56:50 +1200

    :arrow_up: request: Implement required concrete AuthorizeCodeStorage methods

--- request/request_oauth2_authorize_code_storage.go
+++ request/request_oauth2_authorize_code_storage.go
@@ -7,14 +7,17 @@ import (
 
 /* These functions provide a concrete implementation of fosite.AuthorizeCodeStorage */
 
-func (m *MongoManager) CreateAuthorizeCodeSession(ctx context.Context, code string, request fosite.Requester) (err error) {
-	return
+// CreateAuthorizeCodeSession creates a new session for an authorize code grant in mongo
+func (m *MongoManager) CreateAuthorizeCodeSession(_ context.Context, code string, request fosite.Requester) (err error) {
+	return m.createSession(code, request, mongoCollectionAuthorizationCode)
 }
 
-func (m MongoManager) GetAuthorizeCodeSession(ctx context.Context, code string, session fosite.Session) (request fosite.Requester, err error) {
-	return
+// GetAuthorizeCodeSession finds an authorize code grant session in mongo
+func (m MongoManager) GetAuthorizeCodeSession(_ context.Context, code string, session fosite.Session) (request fosite.Requester, err error) {
+	return m.findSessionBySignature(code, session, mongoCollectionAuthorizationCode)
 }
 
-func (m *MongoManager) DeleteAuthorizeCodeSession(ctx context.Context, code string) (err error) {
-	return
+// DeleteAuthorizeCodeSession removes an authorize code session from mongo
+func (m *MongoManager) DeleteAuthorizeCodeSession(_ context.Context, code string) (err error) {
+	return m.deleteSession(code, mongoCollectionAuthorizationCode)
 }
