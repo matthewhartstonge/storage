15eb1c4eaa79ae5e85bea2e59a0419f172ff43de
Matthew Hartstonge <matt@linc-ed.com>
2017-05-26 11:39:00 +1200

    :arrow_up: request: Implement required concrete AuthorizeCodeGrantStorage methods

--- request/request_oauth2_authorize_code_grant_storage.go
+++ request/request_oauth2_authorize_code_grant_storage.go
@@ -8,5 +8,19 @@ import (
 /* These functions provide a concrete implementation of fosite.AuthorizeCodeGrantStorage */
 
 func (m *MongoManager) PersistAuthorizeCodeGrantSession(ctx context.Context, authorizeCode, accessSignature, refreshSignature string, request fosite.Requester) error {
+	if err := m.DeleteAuthorizeCodeSession(ctx, authorizeCode); err != nil {
+		return err
+	} else if err := m.CreateAccessTokenSession(ctx, accessSignature, request); err != nil {
+		return err
+	}
+
+	if refreshSignature == "" {
+		return nil
+	}
+
+	if err := m.CreateRefreshTokenSession(ctx, refreshSignature, request); err != nil {
+		return err
+	}
+
 	return nil
 }
