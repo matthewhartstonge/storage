e5431a4ae271bcaf326e2f0788b64ac40da293cc
Matthew Hartstonge <matt@linc-ed.com>
2017-05-26 11:58:12 +1200

    :arrow_up: request: Implement required concrete ImplicitGrantStorage methods

--- request/request_oauth2_implicit_grant_storage.go
+++ request/request_oauth2_implicit_grant_storage.go
@@ -7,6 +7,7 @@ import (
 
 /* These functions provide a concrete implementation of fosite.ImplicitGrantStorage */
 
+// CreateImplicitAccessTokenSession stores an implicit access token based session in mongo
 func (m *MongoManager) CreateImplicitAccessTokenSession(ctx context.Context, token string, request fosite.Requester) (err error) {
-	return
+	return m.CreateAccessTokenSession(ctx, token, request)
 }
