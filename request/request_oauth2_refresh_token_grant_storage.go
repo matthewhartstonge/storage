a58ab6c166de8d53662d83e6750901cdf0552f4c
Matthew Hartstonge <matt@linc-ed.com>
2017-05-26 12:03:50 +1200

    :arrow_up: request: Implement required concrete PersistRefreshTokenGrantSession methods

--- request/request_oauth2_refresh_token_grant_storage.go
+++ request/request_oauth2_refresh_token_grant_storage.go
@@ -5,6 +5,17 @@ import (
 	"github.com/ory/fosite"
 )
 
+/* These functions provide a concrete implementation of fosite.handler.oauth2.PersistRefreshTokenGrantSession */
+
+// PersistRefreshTokenGrantSession stores a refresh token grant session in mongo
 func (m *MongoManager) PersistRefreshTokenGrantSession(ctx context.Context, requestRefreshSignature, accessSignature, refreshSignature string, request fosite.Requester) (err error) {
-	return
+	if err := m.DeleteRefreshTokenSession(ctx, requestRefreshSignature); err != nil {
+		return err
+	} else if err := m.CreateAccessTokenSession(ctx, accessSignature, request); err != nil {
+		return err
+	} else if err := m.CreateRefreshTokenSession(ctx, refreshSignature, request); err != nil {
+		return err
+	}
+
+	return nil
 }
