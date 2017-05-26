775f170fe115e48f85f448db991b7cc5c8a840f9
Matthew Hartstonge <matt@linc-ed.com>
2017-05-26 17:47:57 +1200

    :arrow_up: request: Implement ResourceOwnerPasswordCredentialsGrantStorage methods

--- request/request_oauth2_resource_owner_password_credentials_grant_storage.go
+++ request/request_oauth2_resource_owner_password_credentials_grant_storage.go
@@ -2,12 +2,21 @@ package request
 
 import (
 	"context"
+	"github.com/ory/fosite"
+	"github.com/pkg/errors"
 )
 
 /* These functions provide a concrete implementation of fosite.ResourceOwnerPasswordCredentialsGrantStorage */
 /* fosite.ResourceOwnerPasswordCredentialsGrantStorage also implements fosite.AccessTokenStorage */
 /* fosite.ResourceOwnerPasswordCredentialsGrantStorage also implements fosite.RefreshTokenStorage */
 
-func (m *MongoManager) Authenticate(ctx context.Context, name string, secret string) (err error) {
-	return
+func (m *MongoManager) Authenticate(ctx context.Context, username string, secret string) (err error) {
+	user, err := m.Users.GetUserByUsername(username)
+	if err != nil {
+		return fosite.ErrNotFound
+	}
+	if err := m.Users.Hasher.Compare(user.GetHashedSecret(), []byte(secret)); err != nil {
+		return errors.New("Invalid credentials")
+	}
+	return nil
 }
