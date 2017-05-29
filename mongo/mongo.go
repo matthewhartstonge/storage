9b42e473bc114b5301f4bbbbe31bdcf4a88f22df
Matthew Hartstonge <matt@linc-ed.com>
2017-05-29 12:13:18 +1200

    :arrow_up: mongo: Update collections to include Cache entries

--- mongo/mongo.go
+++ mongo/mongo.go
@@ -2,10 +2,12 @@ package mongo
 
 const (
 	// Collection Definitions
-	CollectionOpenIDSessions    = "OpenIDConnectSessions"
-	CollectionAccessTokens      = "AccessTokens"
-	CollectionRefreshTokens     = "RefreshTokens"
-	CollectionAuthorizationCode = "AuthorizationCodes"
-	CollectionClients           = "Clients"
-	CollectionUsers             = "Users"
+	CollectionOpenIDSessions     = "OpenIDConnectSessions"
+	CollectionAccessTokens       = "AccessTokens"
+	CollectionRefreshTokens      = "RefreshTokens"
+	CollectionAuthorizationCode  = "AuthorizationCodes"
+	CollectionClients            = "Clients"
+	CollectionUsers              = "Users"
+	CollectionCacheAccessTokens  = "CacheAccessTokens"
+	CollectionCacheRefreshTokens = "CacheRefreshTokens"
 )
