89e0eb9738e92119afa7a14d3690115c41424e9c
Matthew Hartstonge <matt@linc-ed.com>
2017-05-30 09:14:14 +1200

    :arrow_up: cache: Create a generic interface for casting any type to cache

--- cache/cache.go
+++ cache/cache.go
@@ -1,6 +1,16 @@
 package cache
 
-type KeyValue struct {
-	K string `bson:"_id" json:"key"`
-	V string `bson:"value" json:"value"`
+type SessionObject struct {
+	ID        string `bson:"_id" json:"key"`
+	Signature string `bson:"signature" json:"signature"`
+}
+
+// GetKey returns the key of the cached session map
+func (s SessionObject) GetKey() string {
+	return s.ID
+}
+
+// GetValue returns session data as a string
+func (s SessionObject) GetValue() (string, error) {
+	return s.Signature, nil
 }
--- cache/cache_manager.go
+++ cache/cache_manager.go
@@ -5,10 +5,16 @@ type Manager interface {
 	Storer
 }
 
+// Cacher provides a generic interface for storing data in a mongo cache
+type Cacher interface {
+	GetKey() string
+	GetValue() (interface{}, error)
+}
+
 // Storer provides a way to create cache based objects in mongo
 type Storer interface {
-	Create(cacheObject KeyValue, collectionName string) error
-	Get(key string, collectionName string) (*KeyValue, error)
-	Update(cacheObject KeyValue, collectionName string) error
+	Create(cacheObject Cacher, collectionName string) error
+	Get(key string, collectionName string) (*Cacher, error)
+	Update(cacheObject Cacher, collectionName string) error
 	Delete(key string, collectionName string) error
 }
