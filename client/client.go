89289d7d68cedc660f47dd6aa6bf5411a95cc9cd
Matthew Hartstonge <matt@linc-ed.com>
2017-06-01 10:42:14 +1200

    :arrow_up: client: update marshalling to conform to camelcase as per idiomatic JS

--- client/client.go
+++ client/client.go
@@ -9,68 +9,68 @@ import (
 // updates.
 type Client struct {
 	// ID is the id for this client.
-	ID string `bson:"_id" json:"id"`
+	ID string `bson:"_id" json:"id" xml:"id"`
 
 	// Name is the human-readable string name of the client to be presented to the
 	// end-user during authorization.
-	Name string `bson:"client_name" json:"client_name"`
+	Name string `bson:"clientName" json:"clientName" xml:"clientName"`
 
 	// Secret is the client's secret. The secret will be included in the create request as cleartext, and then
 	// never again. The secret is stored using BCrypt so it is impossible to recover it. Tell your users
 	// that they need to write the secret down as it will not be made available again.
-	Secret []byte `bson:"client_secret,omitempty" json:"client_secret,omitempty"`
+	Secret []byte `bson:"clientSecret,omitempty" json:"clientSecret,omitempty" xml:"clientSecret,omitempty"`
 
 	// RedirectURIs is an array of allowed redirect urls for the client, for example:
 	// http://mydomain/oauth/callback.
-	RedirectURIs []string `bson:"redirect_uris" json:"redirect_uris"`
+	RedirectURIs []string `bson:"redirectUris" json:"redirectUris" xml:"redirectUris"`
 
 	// GrantTypes is an array of grant types the client is allowed to use.
 	//
 	// Pattern: client_credentials|authorize_code|implicit|refresh_token
-	GrantTypes []string `bson:"grant_types" json:"grant_types"`
+	GrantTypes []string `bson:"grantTypes" json:"grantTypes" xml:"grantTypes"`
 
 	// ResponseTypes is an array of the OAuth 2.0 response type strings that the client can
 	// use at the authorization endpoint.
 	//
 	// Pattern: id_token|code|token
-	ResponseTypes []string `bson:"response_types" json:"response_types"`
+	ResponseTypes []string `bson:"responseTypes" json:"responseTypes" xml:"responseTypes"`
 
 	// Scope is a string containing a space-separated list of scope values (as
 	// described in Section 3.3 of OAuth 2.0 [RFC6749]) that the client
 	// can use when requesting access tokens.
 	//
 	// Pattern: ([a-zA-Z0-9\.]+\s)+
-	Scopes []string `bson:"scopes" json:"scopes"`
+	Scopes []string `bson:"scopes" json:"scopes" xml:"scopes"`
 
 	// Owner is a string identifying the owner of the OAuth 2.0 Client.
-	Owner string `bson:"owner" json:"owner"`
+	Owner string `bson:"owner" json:"owner" xml:"owner"`
 
 	// PolicyURI is a URL string that points to a human-readable privacy policy document
 	// that describes how the deployment organization collects, uses,
 	// retains, and discloses personal data.
-	PolicyURI string `bson:"policy_uri" json:"policy_uri"`
+	PolicyURI string `bson:"policyUri" json:"policyUri" xml:"policyUri"`
 
 	// TermsOfServiceURI is a URL string that points to a human-readable terms of service
 	// document for the client that describes a contractual relationship
 	// between the end-user and the client that the end-user accepts when
 	// authorizing the client.
-	TermsOfServiceURI string `bson:"tos_uri" json:"tos_uri"`
+	TermsOfServiceURI string `bson:"termsOfServiceUri" json:"termsOfServiceUri" xml:"termsOfServiceUri"`
 
 	// ClientURI is an URL string of a web page providing information about the client.
 	// If present, the server SHOULD display this URL to the end-user in
 	// a clickable fashion.
-	ClientURI string `bson:"client_uri" json:"client_uri"`
+	ClientURI string `bson:"clientUri" json:"clientUri" xml:"clientUri"`
 
 	// LogoURI is an URL string that references a logo for the client.
-	LogoURI string `bson:"logo_uri" json:"logo_uri"`
+	LogoURI string `bson:"logoUri" json:"logoUri" xml:"logoUri"`
 
 	// Contacts is a array of strings representing ways to contact people responsible
 	// for this client, typically email addresses.
-	Contacts []string `bson:"contacts" json:"contacts"`
+	Contacts []string `bson:"contacts" json:"contacts" xml:"contacts"`
 
 	// Public is a boolean that identifies this client as public, meaning that it
 	// does not have a secret. It will disable the client_credentials grant type for this client if set.
-	Public bool `bson:"public" json:"public"`
+	Public bool `bson:"public" json:"public" xml:"public"`
 }
 
 // GetID returns the client's Client ID.
