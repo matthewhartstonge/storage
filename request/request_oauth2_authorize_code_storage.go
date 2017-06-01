1f86c06b50065fdfda9d52227b50c9bc5c092043
Matthew Hartstonge <matt@linc-ed.com>
2017-06-02 11:38:57 +1200

    :arrow_up: mongo: use renamed constant

--- request/request_oauth2_authorize_code_storage.go
+++ request/request_oauth2_authorize_code_storage.go
@@ -10,15 +10,15 @@ import (
 
 // CreateAuthorizeCodeSession creates a new session for an authorize code grant in mongo
 func (m *MongoManager) CreateAuthorizeCodeSession(_ context.Context, code string, request fosite.Requester) (err error) {
-	return m.createSession(code, request, mongo.CollectionAuthorizationCode)
+	return m.createSession(code, request, mongo.CollectionAuthorizationCodes)
 }
 
 // GetAuthorizeCodeSession finds an authorize code grant session in mongo
 func (m MongoManager) GetAuthorizeCodeSession(_ context.Context, code string, session fosite.Session) (request fosite.Requester, err error) {
-	return m.findSessionBySignature(code, session, mongo.CollectionAuthorizationCode)
+	return m.findSessionBySignature(code, session, mongo.CollectionAuthorizationCodes)
 }
 
 // DeleteAuthorizeCodeSession removes an authorize code session from mongo
 func (m *MongoManager) DeleteAuthorizeCodeSession(_ context.Context, code string) (err error) {
-	return m.deleteSession(code, mongo.CollectionAuthorizationCode)
+	return m.deleteSession(code, mongo.CollectionAuthorizationCodes)
 }
