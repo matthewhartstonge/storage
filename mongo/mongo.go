b96b5fbfafbbad6c81d9f098bbade0ca0243b495
Matthew Hartstonge <matt@linc-ed.com>
2017-06-02 11:38:12 +1200

    :arrow_up: mongo: Comment constants

--- mongo/mongo.go
+++ mongo/mongo.go
@@ -1,13 +1,27 @@
 package mongo
 
 const (
-	// Collection Definitions
-	CollectionOpenIDSessions     = "OpenIDConnectSessions"
-	CollectionAccessTokens       = "AccessTokens"
-	CollectionRefreshTokens      = "RefreshTokens"
-	CollectionAuthorizationCode  = "AuthorizationCodes"
-	CollectionClients            = "Clients"
-	CollectionUsers              = "Users"
-	CollectionCacheAccessTokens  = "CacheAccessTokens"
+	// CollectionOpenIDSessions provides the name of the mongo collection to use in order to create, read, update and delete OpenID Sessions
+	CollectionOpenIDSessions = "OpenIDConnectSessions"
+
+	// CollectionOpenIDSessions provides the name of the mongo collection to use in order to create, read, update and delete Access Tokens
+	CollectionAccessTokens = "AccessTokens"
+
+	// CollectionOpenIDSessions provides the name of the mongo collection to use in order to create, read, update and delete Refresh Tokens
+	CollectionRefreshTokens = "RefreshTokens"
+
+	// CollectionAuthorizationCodes provides the name of the mongo collection to use in order to create, read, update and delete Authorization Codes
+	CollectionAuthorizationCodes = "AuthorizationCodes"
+
+	// CollectionClients provides the name of the mongo collection to use in order to create, read, update and delete Clients
+	CollectionClients = "Clients"
+
+	// CollectionUsers provides the name of the mongo collection to use in order to create, read, update and delete Users
+	CollectionUsers = "Users"
+
+	// CollectionCacheAccessTokens provides the name of the mongo collection to use in order to create, read, update and delete Cache Access Tokens
+	CollectionCacheAccessTokens = "CacheAccessTokens"
+
+	// CollectionCacheRefreshTokens provides the name of the mongo collection to use in order to create, read, update and delete Cache Refresh Tokens
 	CollectionCacheRefreshTokens = "CacheRefreshTokens"
 )
