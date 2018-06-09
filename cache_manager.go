aacc1774d2598f5b92a5f390fefcb5f9aa6075b6
Matthew Hartstonge <matt@mykro.co.nz>
2018-06-10 00:53:48 +1200

    :arrow_up: Adds cache to the domain model.

---
+++ cache_manager.go
@@ -0,0 +1,23 @@
+package storage
+
+import "context"
+
+// Cacher provides a generic interface for storing data in a cache
+type Cacher interface {
+	Key() string
+	Value() string
+}
+
+// CacheManager provides a generic interface to key value cache objects in
+// order to build a cache datastore.
+type CacheManager interface {
+	CacheStorer
+}
+
+// Storer provides a way to create cache based objects in mongo
+type CacheStorer interface {
+	Create(ctx context.Context, entityName string, cacheObject SessionCache) (SessionCache, error)
+	Get(ctx context.Context, entityName string, key string) (SessionCache, error)
+	Update(ctx context.Context, entityName string, cacheObject SessionCache) (SessionCache, error)
+	Delete(ctx context.Context, entityName string, key string) error
+}
