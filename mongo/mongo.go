6320b476264e3881328d89ad9d3e55b013864dea
Matthew Hartstonge <matt@mykro.co.nz>
2018-06-13 01:46:09 +1200

    :arrow_up: Add indices and collections for requests.

--- mongo/mongo.go
+++ mongo/mongo.go
@@ -23,32 +23,56 @@ const (
 	IdxUserId = "idxUserId"
 
 	// IdxUsername provides a mongo index based on username
-	IdxUsername = "IdxUsername"
+	IdxUsername = "idxUsername"
+
+	// IdxSessionId provides a mongo index based on Session
+	IdxSessionId = "idxSessionId"
+
+	// IdxSignatureId provides a mongo index based on Signature
+	IdxSignatureId = "idxSignatureId"
+
+	// IdxCompoundRequester provides a mongo compound index based on Client ID
+	// and User ID for when filtering request records.
+	IdxCompoundRequester = "idxCompoundRequester"
 )
 
 const (
-	// CollectionOpenIDSessions provides the name of the mongo collection to use in order to create, read, update and delete OpenID Sessions
+	// CollectionOpenIDSessions provides the name of the mongo collection to
+	// use in order to create, read, update and delete OpenID Sessions.
 	CollectionOpenIDSessions = "openIDConnectSessions"
 
-	// CollectionOpenIDSessions provides the name of the mongo collection to use in order to create, read, update and delete Access Tokens
+	// CollectionOpenIDSessions provides the name of the mongo collection to
+	// use in order to create, read, update and delete Access Tokens sessions.
 	CollectionAccessTokens = "accessTokens"
 
-	// CollectionOpenIDSessions provides the name of the mongo collection to use in order to create, read, update and delete Refresh Tokens
+	// CollectionOpenIDSessions provides the name of the mongo collection to
+	// use in order to create, read, update and delete Refresh Tokens sessions.
 	CollectionRefreshTokens = "refreshTokens"
 
-	// CollectionAuthorizationCodes provides the name of the mongo collection to use in order to create, read, update and delete Authorization Codes
+	// CollectionAuthorizationCodes provides the name of the mongo collection
+	// to use in order to create, read, update and delete Authorization Code
+	// sessions.
 	CollectionAuthorizationCodes = "authorizationCodes"
 
-	// CollectionClients provides the name of the mongo collection to use in order to create, read, update and delete Clients
+	// CollectionPKCESessions provides the name of the mongo collection
+	// to use in order to create, read, update and delete Proof Key for Code
+	// Exchange sessions.
+	CollectionPKCESessions = "pkceSessions"
+
+	// CollectionClients provides the name of the mongo collection to use in
+	// order to create, read, update and delete Clients.
 	CollectionClients = "clients"
 
-	// CollectionUsers provides the name of the mongo collection to use in order to create, read, update and delete Users
+	// CollectionUsers provides the name of the mongo collection to use in
+	// order to create, read, update and delete Users.
 	CollectionUsers = "users"
 
-	// CollectionCacheAccessTokens provides the name of the mongo collection to use in order to create, read, update and delete Cache Access Tokens
+	// CollectionCacheAccessTokens provides the name of the mongo collection
+	// to use in order to create, read, update and delete Cache Access Tokens.
 	CollectionCacheAccessTokens = "cacheAccessTokens"
 
-	// CollectionCacheRefreshTokens provides the name of the mongo collection to use in order to create, read, update and delete Cache Refresh Tokens
+	// CollectionCacheRefreshTokens provides the name of the mongo collection
+	// to use in order to create, read, update and delete Cache Refresh Tokens.
 	CollectionCacheRefreshTokens = "cacheRefreshTokens"
 )
 
