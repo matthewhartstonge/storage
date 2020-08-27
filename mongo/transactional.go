27b8342f9f3d7872b0804f92ff2a4635ab9f3cbf
Matthew Hartstonge <matt@mykro.co.nz>
2020-08-27 21:51:35 +1200

    :zap: mongo: implicitly create a new session for the transaction.
    - this isolates calls that would not provide a mongo session, which would lead to failure that is not friendly to developers.

--- mongo/transactional.go
+++ mongo/transactional.go
@@ -7,19 +7,22 @@ import (
 	// External Imports
 	"github.com/ory/fosite"
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readconcern"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"go.mongodb.org/mongo-driver/mongo/writeconcern"
 )
 
-func (s *Store) BeginTX(ctx context.Context) (context.Context, error) {
-	session, ok := ContextToSession(ctx)
-	if !ok {
-		return ctx, errors.Wrap(
-			fosite.ErrServerError,
-			"transaction failed: no mongo session stored in context",
-		)
+func (s *Store) BeginTX(ctx context.Context) (context context.Context, err error) {
+	session, err := s.DB.Client().StartSession()
+	if err != nil {
+		fields := logrus.Fields{
+			"package": "mongo",
+			"method":  "BeginTX",
+		}
+		logger.WithError(err).WithFields(fields).Error("error starting mongo transaction")
+		return ctx, err
 	}
 
 	// Define mongo transaction options...
@@ -29,7 +32,7 @@ func (s *Store) BeginTX(ctx context.Context) (context.Context, error) {
 		SetWriteConcern(writeconcern.New(writeconcern.WMajority())).
 		SetMaxCommitTime(&s.timeout)
 
-	err := session.StartTransaction(opts)
+	err = session.StartTransaction(opts)
 	if err != nil {
 		_ = session.AbortTransaction(ctx)
 		return ctx, err
@@ -46,6 +49,7 @@ func (s *Store) Commit(ctx context.Context) error {
 			"commit failed: no transaction stored in context",
 		)
 	}
+	defer txn.EndSession(ctx)
 
 	err := txn.CommitTransaction(ctx)
 	if err != nil {
@@ -63,6 +67,7 @@ func (s *Store) Rollback(ctx context.Context) error {
 			"rollback failed: no transaction stored in context",
 		)
 	}
+	defer txn.EndSession(ctx)
 
 	err := txn.AbortTransaction(ctx)
 	if err != nil {
