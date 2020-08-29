e415e71e0c50a4a59aa958c490a99ac976cc3705
Matthew Hartstonge <matt@mykro.co.nz>
2020-08-30 00:33:13 +1200

    :zap: mongo: fixes context storing/getting mongo sessions.
    - the mongo driver used it's own internal context key to store the mongo session, as such the sessions being created in storage weren't being used. This led to non-causally consistent errors occurring when creates happened before deletes on the refresh token handler.

--- mongo/mongo_meta.go
+++ mongo/mongo_meta.go
@@ -32,54 +32,18 @@ const (
 	IdxCompoundRequester = "idxCompoundRequester"
 )
 
-// ctxMongoKey is an unexported type to enable passing mongo information via a
-// context in this package. This prevents collisions with keys defined in other
-// packages.
-type ctxMongoKey int
-
-const (
-	// mongoSessionKey is the key for mongo.Session values in Contexts. It is
-	// unexported; clients should use datastore.SessionToContext and
-	// datastore.ContextToSession instead of attempting to use this key.
-	mongoSessionKey ctxMongoKey = iota
-	// mongoTxnKey provides a context key for storing transaction based sessions
-	// in contexts.
-	mongoTxnKey
-)
-
 // SessionToContext provides a way to push a mongo datastore session into the
 // current context, which can then be passed on to other routes or functions.
 func SessionToContext(ctx context.Context, session mongo.Session) context.Context {
-	return context.WithValue(ctx, mongoSessionKey, session)
+	return mongo.NewSessionContext(ctx, session)
 }
 
 // ContextToSession provides a way to obtain a mongo session, if contained
 // within the presented context.
 func ContextToSession(ctx context.Context) (sess mongo.Session, ok bool) {
-	if tx, ok := ContextToTransaction(ctx); ok {
-		// Always return the transaction session if available.
-		return tx, ok
+	if sess := mongo.SessionFromContext(ctx); sess != nil {
+		return sess, true
 	}
 
-	return ctxToSess(ctx, mongoSessionKey)
-}
-
-// TransactionToContext provides a way to push a mongo transaction-based
-// session into the provided context, which can then be passed on to other
-// routes or functions.
-func TransactionToContext(ctx context.Context, session mongo.Session) context.Context {
-	return context.WithValue(ctx, mongoTxnKey, session)
-}
-
-// ContextToTransaction provides a way to obtain a mongo transaction-based
-// session, if contained within the presented context.
-func ContextToTransaction(ctx context.Context) (sess mongo.Session, ok bool) {
-	return ctxToSess(ctx, mongoTxnKey)
-}
-
-// ctxToSess provides a wrapper around extracting a mongo session from a
-// context.
-func ctxToSess(ctx context.Context, key ctxMongoKey) (sess mongo.Session, ok bool) {
-	sess, ok = ctx.Value(key).(mongo.Session)
-	return
+	return nil, false
 }
