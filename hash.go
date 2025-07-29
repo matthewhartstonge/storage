15a6cdb93a5e27b772fb1f35fd45de5dec91bdf0
Matthew Hartstonge <matt@mykro.co.nz>
2025-07-29 13:10:34 +1200

    refactor(hash): always hash no matter the signature and use hex encode rather than sprintf'ing.

--- hash.go
+++ hash.go
@@ -2,14 +2,12 @@ package storage
 
 import (
 	"crypto/sha512"
-	"fmt"
+	"encoding/hex"
 )
 
 // SignatureHash hashes the signature to prevent errors where the signature is
 // longer than 128 characters.
 func SignatureHash(signature string) string {
-	if signature == "" {
-		return ""
-	}
-	return fmt.Sprintf("%x", sha512.Sum384([]byte(signature)))
+	sum := sha512.Sum384([]byte(signature))
+	return hex.EncodeToString(sum[:])
 }
